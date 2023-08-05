fb16fe95e9316422acd5587bc1cb966d7bcafe1e
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-05 13:45:37 -0300

    add script e css

---
+++ curso js/aula16/script.js
@@ -0,0 +1,18 @@
+function Contar() {
+   
+    /* começo */
+    var ini = document.getElementById("ini")
+    var ini = Number(ini.value)
+
+    /* fim */
+    var fim = document.getElementById("fim")
+    var fim = Number(fim.value)
+
+    /*passo */
+    var pas = document.getElementById("pass")
+    var pas = Number(pas.value)
+
+    var res = document.getElementById("div2")
+
+    
+}
