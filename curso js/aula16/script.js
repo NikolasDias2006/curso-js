ffe2689af85582a5434c523de49bcb8bfa4e9dfe
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-05 22:18:46 -0300

    Contador

--- curso js/aula16/script.js
+++ curso js/aula16/script.js
@@ -1,18 +1,36 @@
 function Contar() {
-   
     /* começo */
-    var ini = document.getElementById("ini")
-    var ini = Number(ini.value)
-
+    var ini = document.getElementById("ini")  
     /* fim */
     var fim = document.getElementById("fim")
-    var fim = Number(fim.value)
-
     /*passo */
     var pas = document.getElementById("pass")
-    var pas = Number(pas.value)
 
     var res = document.getElementById("div2")
+  /* || = operador OU */
 
-    
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0 ) {
+        alert("[ERRO!!]Faltam dados")
+    } else {
+        i = Number(ini.value)
+        f = Number(fim.value)
+        p = Number(pas.value)
+
+       if(p <= 0) {
+        alert("é impossivel contar pulando em 0 em 0 entao acresetei passo 1")
+        p = 1
+       }
+       if(i < f){
+        /*Contagem crescente */
+        for(var c = i;c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+        }
+    } else {
+        /* contagem decresente */
+        for(var c = i;c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+    }
+    }
+    res.innerHTML += `\u{1f3c1}`
+}
 }
