9d5684c9e91abbe0192c6bd05db1936ba6fd1cec
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-08-30 22:22:59 -0300

    teste js

--- curso js Matheus Battisti/praticas/prat1/script.js
+++ curso js Matheus Battisti/praticas/prat1/script.js
@@ -1,9 +1,39 @@
+const tit = document.querySelector(".tit")
+
 // const nao deixa declarar outra variavel.
 const x = "hello world"
-x = 1 // dá erro
+//x = 1 dá erro
 
 let y = "Hle"
 
 console.log(y)
 
-//parei na parte 45:38
+
+for (i=10;i > 0;i--) {
+    console.log("teste "+i)
+}
+
+
+let i1 = 0
+
+while(i1 <= 10) {
+    console.log(i1)
+    i1 ++
+}
+
+//arrow function,outro jeito de declarar uma function
+
+/* const arrowtest = () =>alert("Ola,mundo") 
+
+arrowtest() */
+
+/*ao interagir com o ola mundo*/
+
+tit?.addEventListener("click",function(prev) {
+    tit.innerHTML += "tudo bom"
+})
+
+   
+
+
+//parei na parte 1:01:00
