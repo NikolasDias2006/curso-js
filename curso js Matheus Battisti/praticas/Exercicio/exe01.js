f8292c9755c680cc30cbd0a97a78920fd2e440ea
Nikolas_Dev <131534037+NikolasDias2006@users.noreply.github.com>
2023-09-02 11:09:21 -0300

    Update exe01.js

--- curso js Matheus Battisti/praticas/Exercicio/exe01.js
+++ curso js Matheus Battisti/praticas/Exercicio/exe01.js
@@ -2,12 +2,12 @@
 
 console.log("Ola,mundo")
 
-//transformando uma string para numero e mostrando seu tipo
+//exe 2 transformando uma string para numero e mostrando seu tipo
 let string = "12345"
 let stringtonumber = Number(string)
 console.log(typeof(stringtonumber))
 
-//quantas caracteres tem uma string e quantas palavras possue
+//exe 3 quantas caracteres tem uma string e quantas palavras possue
 
 let frase = "Javascript is the best"
 //caracteres
@@ -17,7 +17,7 @@ console.log(frase.length)
 
 console.log(frase.split(" ").length)
 
-// loops arraw: criar uma array 5 nomes e usar o repetidor for para mostrar cada nome no console
+//exe 4 loops arraw: criar uma array 5 nomes e usar o repetidor for para mostrar cada nome no console
 
 let nomes = ["Barbosa","Dias", "Nikolas","Jaison","Ricardo"]
 
@@ -25,4 +25,27 @@ for (let i = 0;i < nomes.length;i++) {
         console.log(nomes[i])
 }
 
-//parei na parte 1:24:35
+//exe 5 funcoes String e math funcao que recebe um horario e devolve em horario am ou pm
+
+//converter horario
+function conhora(hora24) {
+        const [hora,minuto] = hora24.split(":")
+
+        const hora12 = hora % 12 || 12
+        //de manha
+        let periodo = "AM"
+
+        if(hora >= 12) {
+         periodo = "PM"
+        }
+
+        console.log(`${hora12}:${minuto} ${periodo}`)
+} 
+
+conhora("15:16")
+conhora("09:46")
+conhora("20:30")
+conhora("12:00")
+conhora("2:00")
+
+//parei na parte 1:33:35
